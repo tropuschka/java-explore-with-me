fae84ee63b7812c2899117fe75632b89967e982f
Tropuschka <tryan.u@mail.ru>
2025-10-13 17:09:52 +0300

    checkstyle

--- ewm-stat-service/stat_dto/src/main/java/ru/practicum/dto/StatDto.java
+++ ewm-stat-service/stat_dto/src/main/java/ru/practicum/dto/StatDto.java
@@ -1,10 +1,6 @@
 package ru.practicum.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
