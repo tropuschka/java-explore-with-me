80c1e166480865c82b3a22c83331a060bace747e
Tropuschka <tryan.u@mail.ru>
2025-10-18 13:05:31 +0300

    bugfix

--- ewm-stat-service/stat_dto/src/main/java/ru/practicum/dto/StatDto.java
+++ ewm-stat-service/stat_dto/src/main/java/ru/practicum/dto/StatDto.java
@@ -1,13 +1,10 @@
 package ru.practicum.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.practicum.validation.Validation;
-
-import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -20,6 +17,6 @@ public class StatDto {
     @NotBlank(groups = Validation.Create.class)
     private String ip;
     @NotNull(groups = Validation.Create.class)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime timestamp;
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private String timestamp;
 }
--- ewm-stat-service/stat_server/src/main/java/ru/practicum/mapping/StatMapper.java
+++ ewm-stat-service/stat_server/src/main/java/ru/practicum/mapping/StatMapper.java
@@ -3,6 +3,9 @@ package ru.practicum.mapping;
 import ru.practicum.model.Stat;
 import ru.practicum.dto.StatDto;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class StatMapper {
     public static Stat toStat(StatDto statDto) {
         Stat stat = new Stat();
@@ -10,11 +13,12 @@ public class StatMapper {
         stat.setIp(statDto.getIp());
         stat.setApp(statDto.getApp());
         stat.setUri(statDto.getUri());
-        stat.setTimestamp(statDto.getTimestamp());
+        stat.setTimestamp(LocalDateTime.parse(statDto.getTimestamp(),
+                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
         return stat;
     }
 
     public static StatDto toStatDto(Stat stat) {
-        return new StatDto(stat.getId(), stat.getApp(), stat.getUri(), stat.getIp(), stat.getTimestamp());
+        return new StatDto(stat.getId(), stat.getApp(), stat.getUri(), stat.getIp(), stat.getTimestamp().toString());
     }
 }
